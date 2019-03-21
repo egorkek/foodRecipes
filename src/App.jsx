47601841fad00dd3d43e576e5947b51fe8ad7e51
Егор <patron6813@gmail.com>
2019-03-21 20:14:21 +0300

    added images

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import classes from './App.css'
+import image from './img/day.jpg'
 
 class App extends React.Component{
     lol = ()=>{
@@ -8,6 +9,7 @@ class App extends React.Component{
     render(){
         return(
             <div className={classes.App} onClick={this.lol}>
+                <img src={image} alt=""/>
                 asdas
             </div>
         )
--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,14 @@ module.exports ={
                 test: /\.css$/,
                 use: styleLoaders(),
             },
+            {
+                test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+                loader: require.resolve('url-loader'),
+                options: {
+                    limit: 10000,
+                    name: 'static/media/[name].[hash:8].[ext]',
+                },
+            }
         ]
     },
     resolve: {

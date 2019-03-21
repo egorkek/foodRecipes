ded35791ddcf9131251ce88959569bf9d63765f5
Егор <patron6813@gmail.com>
2019-03-21 20:05:03 +0300

    webpack config

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import classes from './App.css'
 
 class App extends React.Component{
-
+    lol = ()=>{
+        console.log('asdas')
+    }
     render(){
         return(
-            <div className={classes.App}>
+            <div className={classes.App} onClick={this.lol}>
                 asdas
             </div>
         )
